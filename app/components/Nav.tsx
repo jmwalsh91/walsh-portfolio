e1f84b21c5541674a59cf10cc282ff50604cbd5b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-02 02:33:38 -0600

    task: update mantine, update ssr style injection with remix package, change navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,11 +1,101 @@
-import React from 'react'
-import { Navbar } from '@mantine/core'
-type Props = {}
+import { useState } from 'react';
+import { Navbar, Center, Tooltip, UnstyledButton, createStyles, Stack } from '@mantine/core';
+import {
+  TablerIcon,
+  IconHome2,
+  IconGauge,
+  IconDeviceDesktopAnalytics,
+  IconFingerprint,
+  IconCalendarStats,
+  IconUser,
+  IconSettings,
+  IconLogout,
+  IconSwitchHorizontal,
+} from '@tabler/icons';
 
-function Nav({}: Props) {
-  return (
-    <Navbar>NavBar</Navbar>
-  )
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    width: 50,
+    height: 50,
+    borderRadius: theme.radius.md,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.white,
+    opacity: 0.85,
+
+    '&:hover': {
+      opacity: 1,
+      backgroundColor: theme.fn.lighten(
+        theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background,
+        0.1
+      ),
+    },
+  },
+
+  active: {
+    opacity: 1,
+    '&, &:hover': {
+      backgroundColor: theme.fn.lighten(
+        theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background,
+        0.15
+      ),
+    },
+  },
+}));
+
+interface NavbarLinkProps {
+  icon: TablerIcon;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
 }
 
-export default Nav
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  const { classes, cx } = useStyles();
+  return (
+    <Tooltip label={label} position="right" transitionDuration={0}>
+      <UnstyledButton onClick={onClick} className={cx(classes.link, { [classes.active]: active })}>
+        <Icon stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const mockdata = [
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconDeviceDesktopAnalytics, label: 'Analytics' },
+  { icon: IconCalendarStats, label: 'Releases' },
+  { icon: IconUser, label: 'Account' },
+];
+
+export default function Nav() {
+  const [active, setActive] = useState(2);
+
+  const links = mockdata.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => setActive(index)}
+    />
+  ));
+
+  return (
+  <>
+      <Navbar.Section grow mt={50}>
+        <Stack justify="center" spacing={0}>
+          {links}
+        </Stack>
+      </Navbar.Section>
+      <Navbar.Section>
+        <Stack justify="center" spacing={0}>
+          <NavbarLink icon={IconSwitchHorizontal} label="Change account" />
+          <NavbarLink icon={IconLogout} label="Logout" />
+        </Stack>
+      </Navbar.Section>
+  </>
+
+  );
+}
--- app/components/Shell.tsx
+++ app/components/Shell.tsx
@@ -16,11 +16,13 @@ import {
 
 import bg from '~/assets/CloudBG.jpg'
 import { Link } from '@remix-run/react';
+import Nav from './Nav';
 export default function AppShellDemo({children}: any) {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
   return (
     <AppShell
+    fixed={true}
       styles={{
         main: {
           background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
@@ -28,40 +30,9 @@ export default function AppShellDemo({children}: any) {
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
-      fixed
+   
       navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-          <Text>Application navbar</Text>
-          <Link to="/">
-            Home
-          </Link>
-          <Link to="projects">
-            Projects
-          </Link>
-          <Link to="blog">
-            Blog
-          </Link>
-        </Navbar>
-      }
-  
-      
-      
-      header={
-        <Header height={70} p="md">
-          <div style={{ position: 'absolute', display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Jordan Walsh Portfolio</Text>
-          </div>
-        </Header>
+        <Nav/>
       }
     >
         {
