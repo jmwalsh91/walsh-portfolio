3f597f476a2484fbf9c8e0b194606548e5133751
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-03 15:24:50 -0600

    refactor: move theme to own file and refactor

--- app/components/Surface.tsx
+++ app/components/Surface.tsx
@@ -1,18 +1,20 @@
-import { Paper, Text } from '@mantine/core'
+import { Paper, Text, ThemeIcon, useMantineTheme } from '@mantine/core'
 import React from 'react'
 
 type Props = {}
 
 function Surface({}: Props) {
+    const theme = useMantineTheme()
   return (
     <Paper
+    shadow='xl'
     sx={{
         backgroundColor: 'black',
         opacity: 0.8,
         width: "450px",
         height: "450px",
         borderRadius: "14px"
-    }}><Text color="blue">Surface</Text>Surface</Paper>
+    }}><Text color={'dimmed'}>Surface</Text>Surface</Paper>
   )
 }
 
--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import {
 import { useState } from "react";
 import Shell from "./components/Shell";
 import FontLoader from "./utils/loaders/FontLoader";
-
+import { theme } from "./styles/theme";
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -53,71 +53,7 @@ function MantineTheme({ children }: { children: React.ReactNode }) {
     >
       <FontLoader/> 
       <MantineProvider
-        theme={{
-          headings: {
-            fontFamily: 'Smoking Typeface'
-          },
-          colorScheme: colorScheme,
-          colors: {
-            'primary': [
-              "#defdfd",
-              "#bbf2f1",
-              "#95e7e7",
-              "#6ededd",
-              "#49d4d3",
-              "#31bbb9",
-              "#219190",
-              "#136867",
-              "#003f3f",
-              "#001717",
-            ],
-            'secondary': [
-              "#ffe3fd",
-              "#f8b2ff",
-              "#ec80ff",
-              "#f04efe",
-              "#fa1ffd",
-              "#e409db",
-              "#b2039e",
-              "#800068",
-              "#4e003b",
-              "#1e0015",
-            ],
-            'accent': [
-              "#fffada",
-              "#fff0ad",
-              "#ffe67d",
-              "#ffdb4b",
-              "#ffd11a",
-              "#e6b800",
-              "#b38f00",
-              "#806600",
-              "#4e3d00",
-              "#1d1400",
-            ],
-            'success': [
-              "#e0fdf3",
-              "#baf2dc",
-              "#94e9c3",
-              "#6bdfa7",
-              "#43d68a",
-              "#29bc7a",
-              "#1d9266",
-              "#11684e",
-              "#044033",
-              "#00170e",
-            ],
-         
-          },
-          other: {
-            accent: "#ffd11a",
-          },
-          primaryColor: 'primary',
-          primaryShade: 5,
-        }}
-        
-        withNormalizeCSS
-        withGlobalStyles
+        theme={theme}
       >
         <Shell fixed position={{ top: 0, left: 0 }}>
           <Outlet />
