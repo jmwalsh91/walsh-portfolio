f7cf7633c28cb58582a3b71230632e345d9aff35
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-05 03:41:13 -0600

    task: rolecard component, stories, rolecards in aboutgrid

---
+++ app/components/cards/RoleCard.tsx
@@ -0,0 +1,55 @@
+import { Paper, Text, ThemeIcon, useMantineTheme } from "@mantine/core";
+import { IconColorSwatch, TablerIcon } from "@tabler/icons";
+import { IconLeaf } from "@tabler/icons";
+import React from "react";
+import { theme } from "~/styles/theme";
+
+type Props = {
+  title: string;
+  description: string;
+};
+interface IconGradient {
+  deg: number;
+  from: string;
+  to: string;
+}
+export type RoleCardProps = {
+  title: string,
+  description: string,
+  icon: TablerIcon
+  gradient: IconGradient;
+};
+function RoleCards({ title, description, icon, gradient }: RoleCardProps) {
+  const theme = useMantineTheme();
+  return (
+    <Paper
+      withBorder
+      radius="md"
+      sx={{
+        backgroundColor: "rgba(255, 255, 255, 0.05)",
+        backdropFilter: "saturate(200%) blur(16px) brightness(130%)",
+        boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
+        borderRadius: "14px",
+   
+        padding: "1rem",
+      }}
+    >
+      <ThemeIcon
+        size="xl"
+        radius="md"
+        variant="gradient"
+        gradient={gradient}
+      >
+        {icon}
+      </ThemeIcon>
+      <Text size="xl" weight={500} mt="md" color={theme.white}>
+        {title}
+      </Text>
+      <Text size="md" mt="sm" align="left" color={theme.white}>
+        {description}
+      </Text>
+    </Paper>
+  );
+}
+
+export default RoleCards;
--- app/components/layouts/AboutGrid.tsx
+++ app/components/layouts/AboutGrid.tsx
@@ -1,34 +1,58 @@
-import { Container, Grid, SimpleGrid, Skeleton, useMantineTheme } from '@mantine/core'
+import { Container, Grid, SimpleGrid, Skeleton, Stack, Text, Title, useMantineTheme } from '@mantine/core'
+import { IconLeaf, IconMicroscope } from '@tabler/icons';
 import React from 'react'
 import Headshot from '../cards/Headshot';
 import { LeadingProject } from '../cards/LeadingProject';
+import RoleCards from '../cards/RoleCard';
 import Surface from '../Surface';
-
-
+import {Top} from '../Top'
 
 
 function AboutGrid() {
 const theme = useMantineTheme();
 
 return (
-    <Container my="md">
-    <Grid>
-        <Grid.Col span={6}>
+    <Container fluid my="md" sx={{
+        height: "300vh"
+    }}>
+{/*       <div dangerouslySetInnerHTML={{__html: `${Top}`}}/> */}
+    <Grid sx={{
+        width: "100%",
+        marginTop: "10vh"
+    }}>
+        <Grid.Col span={4}>
         <Headshot/>
         </Grid.Col>
-    <Grid.Col span={6}>
+    <Grid.Col span={8}>
         <Grid gutter="md" justify="around">
         <Grid.Col span={12}>
-            <Surface width="28rem" height="10rem">Description</Surface>
+            <Stack>
+                <Title>Jordan Walsh</Title>
+          
+                <Surface width="100%" height="5rem">
+                </Surface>
+        </Stack>
+        </Grid.Col>
+        
+        <Grid.Col span={4}>
+    <RoleCards title='React Developer'
+   description='Implements modern React architecture and best practices while maintaining a familiarity with legacy methodologies. Translates pre-16.8 codebases into fresh, concurrent-mode enabled @latest.'
+   icon={IconLeaf}
+   gradient={{ deg: 0, from: 'cyan', to: 'pink' }}/>
         </Grid.Col>
         <Grid.Col span={4}>
-            <Surface width="7rem" height="10rem">Card</Surface>
+        <RoleCards  
+        title='UI Engineer'
+    description='Approaches application architecture and UI design with empathy and knowledge of assistive technologies and W3C and WAI-ARIA standards. Delivers a rad experience to users of all stripes.'
+    icon={IconMicroscope}
+    gradient={{ deg: 17, from: 'pink', to: 'purple' }}/>
         </Grid.Col>
         <Grid.Col span={4}>
-            <Surface width="7rem" height="10rem">Card2</Surface>
-        </Grid.Col>
-        <Grid.Col span={4}>
-            <Surface width="7rem" height="10rem">Card3</Surface>
+        <RoleCards  
+        title='Full-Stack Typescript'
+    description='API development with Node and Typescript. JSDOC/TSDOC for documentation. Leverages Docker to quickly implement and iterate on backend systems to meet data and UI requirements.'
+    icon={IconMicroscope}
+    gradient={{ deg: 17, from: 'pink', to: 'purple' }}/>
         </Grid.Col>
 </Grid>
     </Grid.Col>
