1c6adc4dad38293fcfd7d227e71d9f4e134d69e4
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-05 22:13:44 -0600

    style: base animation for whileinview in aboutgrid

--- app/components/layouts/AboutGrid.tsx
+++ app/components/layouts/AboutGrid.tsx
@@ -1,118 +1,171 @@
-import { Container, Grid, Group, SimpleGrid, Skeleton, Stack, Text, Title, useMantineTheme } from '@mantine/core'
-import { IconLeaf, IconMicroscope } from '@tabler/icons';
-import React from 'react'
-import Headshot from '../cards/Headshot';
-import { LeadingProject } from '../cards/LeadingProject';
-import RoleCards from '../cards/RoleCard';
-import Surface from '../Surface';
-import {Top} from '../Top'
-
+import {
+  Container,
+  Grid,
+  Group,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconLeaf, IconMicroscope } from "@tabler/icons";
+import { motion } from "framer-motion";
+import React from "react";
+import Headshot from "../cards/Headshot";
+import { LeadingProject } from "../cards/LeadingProject";
+import RoleCards from "../cards/RoleCard";
+import Surface from "../Surface";
+import { Top } from "../Top";
 
 function AboutGrid() {
-const theme = useMantineTheme();
+  const theme = useMantineTheme();
 
-return (
-    <Container fluid my="md" sx={{
-        height: "300vh"
-    }}>
-{/*       <div dangerouslySetInnerHTML={{__html: `${Top}`}}/> */}
-    <Grid sx={{
-        width: "100%",
-        marginTop: "10vh"
-    }}>
+  return (
+    <Container
+      fluid
+      my="md"
+      sx={{
+        height: "300vh",
+      }}
+    >
+      {/*       <div dangerouslySetInnerHTML={{__html: `${Top}`}}/> */}
+      <Grid
+        sx={{
+          width: "100%",
+          marginTop: "10vh",
+        }}
+      >
         <Grid.Col span={4}>
-            <div style={{
-                height: "100%",
-                borderRadius: "1rem",
-                paddingTop: "1rem",
-                border: "rgba(132, 59, 206, 1)",
-                boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
-             
-
-            }}>
-        <Headshot/>
-
-            </div>
+          <div
+            style={{
+              height: "100%",
+              borderRadius: "1rem",
+              paddingTop: "1rem",
+              border: "rgba(132, 59, 206, 1)",
+              boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
+            }}
+          >
+            <Headshot />
+          </div>
         </Grid.Col>
-    <Grid.Col span={8}>
-        <Grid gutter="md" justify="around">
-        <Grid.Col span={12}>
-            <Stack>
+        <Grid.Col span={8}>
+          <Grid gutter="md" justify="around">
+            <Grid.Col span={12}>
+              <Stack>
                 <Title>Jordan Walsh</Title>
-          
+
                 <Surface width="100%" height="5rem">
-                    <Text color={theme.white} size="xl" sx={{padding: "1rem"}}>I can do all of the following things like a goddamn boss</Text>
+                  <Text color={theme.white} size="xl" sx={{ padding: "1rem" }}>
+                    I can do all of the following things like a goddamn boss
+                  </Text>
                 </Surface>
+              </Stack>
+            </Grid.Col>
+
+            <Grid.Col span={4}>
+              <RoleCards
+                title="React Developer"
+                description="Implements modern React architecture and best practices while maintaining a familiarity with legacy methodologies. Translates pre-16.8 codebases into fresh, concurrent-mode enabled @latest."
+                icon={IconLeaf}
+                gradient={{ deg: 0, from: "cyan", to: "pink" }}
+              />
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <RoleCards
+                title="UI Engineer"
+                description="Approaches application architecture and UI design with empathy and knowledge of assistive technologies and W3C/WAI-ARIA standards. Delivers a rad experience to users of all stripes."
+                icon={IconMicroscope}
+                gradient={{ deg: 17, from: "pink", to: "purple" }}
+              />
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <RoleCards
+                title="Full-Stack Typescript"
+                description="API development with Node and Typescript. JSDOC/TSDOC for documentation. Leverages Docker to quickly implement and iterate on backend systems to meet data and UI requirements."
+                icon={IconMicroscope}
+                gradient={{ deg: 17, from: "pink", to: "purple" }}
+              />
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+      </Grid>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          margin: 100
+        }}
+        transition={{
+            duration: .4
+        }}
+        style={{
+          borderRadius: "1rem",
+          padding: "3rem",
+          border: "1px solid rgba(132, 59, 206, .4)",
+          boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
+          marginTop: "10rem",
+        }}
+      >
+        <Stack>
+          <Title>Rad stuff</Title>
+          <Group>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+          </Group>
         </Stack>
-        </Grid.Col>
-        
-        <Grid.Col span={4}>
-    <RoleCards title='React Developer'
-   description='Implements modern React architecture and best practices while maintaining a familiarity with legacy methodologies. Translates pre-16.8 codebases into fresh, concurrent-mode enabled @latest.'
-   icon={IconLeaf}
-   gradient={{ deg: 0, from: 'cyan', to: 'pink' }}/>
-        </Grid.Col>
-        <Grid.Col span={4}>
-        <RoleCards  
-        title='UI Engineer'
-    description='Approaches application architecture and UI design with empathy and knowledge of assistive technologies and W3C/WAI-ARIA standards. Delivers a rad experience to users of all stripes.'
-    icon={IconMicroscope}
-    gradient={{ deg: 17, from: 'pink', to: 'purple' }}/>
-        </Grid.Col>
-        <Grid.Col span={4}>
-        <RoleCards  
-        title='Full-Stack Typescript'
-    description='API development with Node and Typescript. JSDOC/TSDOC for documentation. Leverages Docker to quickly implement and iterate on backend systems to meet data and UI requirements.'
-    icon={IconMicroscope}
-    gradient={{ deg: 17, from: 'pink', to: 'purple' }}/>
-        </Grid.Col>
-</Grid>
-    </Grid.Col>
-    </Grid>
-    <div style={{
-             
-                borderRadius: "1rem",
-                padding: "3rem",
-                border: "1px solid rgba(132, 59, 206, .4)",
-                boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
-                marginTop: "10rem"
-
-            }}>
-    <Stack  
-      > 
-        <Title>Rad stuff</Title>
-        <Group>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-        </Group>
-    </Stack>
-    </div>
-    <div style={{
-             
-             borderRadius: "1rem",
-             padding: "3rem",
-             border: "1px solid rgba(132, 59, 206, .4)",
-             boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
-             marginTop: "10rem",
-             clipPath: "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)",
-         
-
-         }}>
-                <Stack> 
-        <Title>Rad stuff</Title>
-        <Group>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-            <Surface width="15rem" height="12rem" >Hey</Surface>
-        </Group>
+      </motion.div>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          margin: 100
+        }}
+        transition={{
+            duration: .4
+        }}
+        style={{
+          borderRadius: "1rem",
+          padding: "3rem",
+          border: "1px solid rgba(132, 59, 206, .4)",
+          boxShadow: "rgba(132, 59, 206, 0.15) 0px 4px 24px 0px",
+          marginTop: "10rem",
+        }}
+      >
+        <Stack>
+          <Title>Rad stuff</Title>
+          <Group>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+            <Surface width="15rem" height="12rem">
+              Hey
+            </Surface>
+          </Group>
         </Stack>
-            </div>
-  </Container>
-
-  )
+      </motion.div>
+    </Container>
+  );
 }
 
-export default AboutGrid
+export default AboutGrid;
