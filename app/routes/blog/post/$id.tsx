e0f5f335eadb37c91ff0a7b2b383b909ff74fe5b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-07 03:05:34 -0600

    task: blogpost nested route

---
+++ app/routes/blog/post/$id.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function $id({}: Props) {
+  return (
+    <div>$id match!</div>
+  )
+}
+
+export default $id
---
+++ app/routes/blog/post/index.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from '@remix-run/react'
+import React from 'react'
+
+type Props = {}
+
+function index({}: Props) {
+  return (
+    <div>post</div>
+  )
+}
+
+export default index
