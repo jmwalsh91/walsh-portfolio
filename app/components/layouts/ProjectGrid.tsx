ecf8b4f43281bba76577e0e6d20fa76c6bd47608
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-08 03:07:18 -0600

    task: loader with getAllProjects in ProjectGrid

--- app/components/layouts/ProjectGrid.tsx
+++ app/components/layouts/ProjectGrid.tsx
@@ -1,31 +1,23 @@
 import { Container, Grid, SimpleGrid, Skeleton, useMantineTheme } from '@mantine/core'
+import { json, LoaderFunction } from '@remix-run/node';
 import React from 'react'
+import { sb } from '~/services/sb';
 import { LeadingProject } from '../cards/LeadingProject';
 
 
-const PRIMARY_COL_HEIGHT = 360;
+export const loader: LoaderFunction = async () => {
+  //TODO: ERROR Handlers
+  const allProjects = await sb.getAllProjects()
+  console.log(allProjects)
+  return json({allProjects: allProjects})
+};
 
 function ProjectGrid() {
 const theme = useMantineTheme();
-const SECONDARY_COL_HEIGHT = PRIMARY_COL_HEIGHT / 2 - theme.spacing.md / 2;
+
 return (
     <Container my="md">
-    <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-<LeadingProject title="cryptones" image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrO7PqNNqAc39QGOWVqWFAiRPmvJn3iw76Lw&usqp=CAU'} category={'NFT'} description="Unique compositions generated from cryptocurrency price data"/>
-      {/* <Skeleton height={PRIMARY_COL_HEIGHT} width="20rem"  radius="md" animate={true} /> */}
-      <Grid gutter="md">
-        <Grid.Col>
-          <Skeleton height={SECONDARY_COL_HEIGHT} width="20rem" radius="md" animate={true} />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Skeleton height={SECONDARY_COL_HEIGHT} width="8rem" radius="md" animate={true} />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Skeleton height={SECONDARY_COL_HEIGHT} width="8rem" radius="md" animate={true} />
-    
-        </Grid.Col>
-      </Grid>
-    </SimpleGrid>
+    <LeadingProject image={''} title={''} category={''} description={''} />
   </Container>
 
   )
