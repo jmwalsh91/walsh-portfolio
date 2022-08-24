d8ca927fd256be2577e2716e76c7b07a5b489569
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-23 23:59:41 -0600

    task: copybutton

--- app/components/XsNav.tsx
+++ app/components/XsNav.tsx
@@ -33,58 +33,18 @@ function XsNav({opened, setOpened}: Props) {
     padding="xl"
     size="xs"
   >
-    <Stack justify="space-around" sx={{
-        height: "100vh"
-    }}>
-<Container>
 
 
-
-    <Button component='a' href="https://github.com/jmwalsh91" target="_blank" rel="noopener noreferrer" variant="outline" size={'xl'} sx={{
-      textDecoration: 'none',
-    borderRadius: theme.radius.md,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.white,
-    opacity: 0.85,
-    padding: ".5rem"
-    }}>
-      <IconBrandGithub/>
-      GitHub</Button>
-      <Button component='a' href="https://github.com/jmwalsh91" variant="outline" size={'xl'} sx={{
-      textDecoration: 'none',
-    borderRadius: theme.radius.md,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.white,
-    opacity: 0.85,
-    padding: ".5rem"
-    }}>
-      <IconBrandGithub/>
-      LinkedIn</Button>
-      <Button component='a' href="https://github.com/jmwalsh91" variant="outline" size={'xl'} >
-      <IconBrandGithub/>
-      Resume</Button>
-      <ExternalLinkButton icon={IconBrandGithub} label="Github" url="https://github.com/jmwalsh91" />
-    <Text size={'xl'}>
-      Resume</Text>
-
-  
-    <Text size={'xl'}>Resume</Text>
-</Container>
  
-    <Container>
+   
     <Text>Social</Text>
       <Button variant="subtle" size="lg" component='a' href="https://github.com/jmwalsh91" target="_blank" rel="noopener noreferrer"><IconBrandGithub size={20} /> Github</Button>
       <Button variant="subtle" size="lg"><IconBrandLinkedin size={20} /> LinkedIn</Button>
       <Button variant="subtle" size="lg"><IconBrandMastodon size={20} /> Mastodon</Button>
-      <Button
-      >
-      <IconBrandGmail size={20} />
+    
+
         <CopyEmail/>
-      </Button>
+  
 
       <Divider />
       <Text>Resources</Text>
@@ -92,8 +52,8 @@ function XsNav({opened, setOpened}: Props) {
       <Button variant="subtle" size="lg"><IconBookDownload size={20} /> Resume</Button>
       <Button variant="subtle" size="lg"><IconBriefcase size={20} /> Doc Kit</Button>
   
-    </Container>
-        </Stack>
+
+
   </Drawer>
   <Group position="center">
   </Group>
