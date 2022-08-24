4cad1e66746835face1b5ae5d0bfc387b50aab1b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-08-24 00:10:32 -0600

    refactor: CopyEmail

--- app/components/buttons/CopyEmail.tsx
+++ app/components/buttons/CopyEmail.tsx
@@ -1,17 +1,21 @@
-import { Button, CopyButton } from '@mantine/core'
+import { Button, CopyButton, useMantineTheme } from '@mantine/core'
 import { useClipboard } from '@mantine/hooks'
 
 import React from 'react'
-
+import {IconBrandGmail, IconClipboardCheck, IconClipboardCopy} from '@tabler/icons'
+import { theme } from '~/styles/theme'
 type Props = {}
 
 function CopyEmail({}: Props) {
-    const clipboard = useClipboard({timeout: 500})
+    const theme = useMantineTheme()
+    const clipboard = useClipboard({timeout: 1000})
   return (
-    <CopyButton value="https://mantine.dev">
+    <CopyButton value="jordan@jmwalsh.dev">
       {({ copied, copy }) => (
-        <Button color={copied ? 'teal' : 'blue'} onClick={() => clipboard.copy('jordan@jmwalsh.dev')}>
-          {copied ? 'Copied email' : 'Copy email'}
+        <Button variant="subtle" color={copied ? theme.colors.success[2] : theme.colors.primary[5]} onClick={copy}>
+            
+          {copied ?<IconClipboardCheck size={20}/> : <IconClipboardCopy size={20}/>}
+          {copied ? 'Copied Email!' : 'Copy Email'}
         </Button>
       )}
     </CopyButton>
